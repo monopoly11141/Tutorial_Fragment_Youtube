8a537fdae2444e3c4e52fd16f2bce2343d805125
monopoly11141 <monopoly1114@vt.edu>
2023-03-27 17:41:44 +0900

    +Fragments
    Added FrameLayout and buttons to display different Fragments.

--- app/src/main/java/com/example/tutorial_fragment_youtube/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_fragment_youtube/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.tutorial_fragment_youtube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.tutorial_fragment_youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            addToBackStack(null)
+            commit()
+        }
+
+        binding.btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
